7cae433f5e730997817a845dba28b3121897e294
HAncycyLee <hancylee7@gmail.com>
2025-02-04 14:28:40 +0800

    添加了不需要跟踪的文件

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    // 自定义规则可以在这里添加
+  }
+}
